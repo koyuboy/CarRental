1b38ee25433e588441af83048fe2042e94865a70
koyuboy <35155252+koyuboy@users.noreply.github.com>
2021-02-25 22:41:31 +0300

    Autofac-FluentValidation -AOP eklendi 25.02.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -17,14 +20,15 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-            }
-            return new ErrorResult(Messages.CarDailyPriceInvalid);            
+
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
+
+            //return new ErrorResult(Messages.CarDailyPriceInvalid);            
         }
 
         public IResult Delete(Car car)
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,7 +30,8 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
+           
+            /*services.AddSingleton<ICarService, CarManager>();
             services.AddSingleton<ICarDal, EfCarDal>();
             services.AddSingleton<IBrandService, BrandManager>();
             services.AddSingleton<IBrandDal, EfBrandDal>();
@@ -41,7 +42,7 @@ namespace WebAPI
             services.AddSingleton<ICustomerService, CustomerManager>();
             services.AddSingleton<ICustomerDal, EfCustomerDal>();
             services.AddSingleton<IColorService, ColorManager>();
-            services.AddSingleton<IColorDal, EfColorDal>();
+            services.AddSingleton<IColorDal, EfColorDal>();*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
